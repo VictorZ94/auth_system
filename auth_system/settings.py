7703c3726c0cd35fbcb4bf001a1d1404f6287f48
Victor Zuluaga <zrvictor@outlook.com>
2024-02-26 20:26:01 -0500

    added datetime

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
