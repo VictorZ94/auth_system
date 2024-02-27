a722faf7bbe1e8e19a0bb1935aa750ca9d42533a
Victor Zuluaga <zrvictor@outlook.com>
2024-02-26 20:24:48 -0500

    rename activate account

--- frontend/src/pages/Activate.jsx
+++ frontend/src/pages/Activate.jsx
@@ -2,15 +2,26 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Button } from "flowbite-react";
 import { FaCheckCircle } from "react-icons/fa";
-import { activate } from "../actions/auth";
+import { activateAccount } from "../actions/auth";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
-const Activate = ({ activate }) => {
+const Activate = ({ activateAccount }) => {
+  const [accountActivated, setAccountActivated] = useState(false);
   const { uid, token } = useParams();
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    activate(uid, token);
+    console.log("enter into activate");
+    activateAccount(uid, token);
+    setAccountActivated(true);
   };
+
+  useEffect(() => {
+    if (accountActivated) {
+      toast.success("Account activated successfully!");
+    }
+  }, [accountActivated]);
 
   return (
     <div className="mt-14 mx-auto max-w-md mb-5 text-center">
@@ -20,10 +31,15 @@ const Activate = ({ activate }) => {
       </h1>
       <p className="my-10">Please activate your Account</p>
       <form onSubmit={handleOnSubmit} className="flex justify-center">
-        <Button size={"xl"}>ACTIVATE</Button>
+        <Button type="submit" size={"xl"}>
+          ACTIVATE
+        </Button>
       </form>
+      <div>
+        <ToastContainer position="bottom-right" draggable />
+      </div>
     </div>
   );
 };
 
-export default connect(null, { activate })(Activate);
+export default connect(null, { activateAccount })(Activate);
