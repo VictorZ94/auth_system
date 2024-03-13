3e45b8310bddd6c22bc118d80023790567e7eedd
Victor Zuluaga <zrvictor@outlook.com>
2024-03-13 17:39:38 -0500

    other changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.jsx";
 
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./utils/store";
 import { Flowbite } from "flowbite-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers";
-
-const initialState = {};
-
-export default configureStore({
-  reducer: rootReducer,
-  initialState,
-});
