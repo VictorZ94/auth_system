21cc3ac2a393da886d342ff8662a24b1e520ca30
Victor Zuluaga <zrvictor@outlook.com>
2024-03-13 17:39:21 -0500

    protect home view

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../actions/auth";
-import { Button, DarkThemeToggle, Navbar as NavbarFlow } from "flowbite-react";
+import { DarkThemeToggle, Navbar as NavbarFlow } from "flowbite-react";
 
 const Navbar = ({ logout, isAuthenticated }) => {
   const guestLinks = () => (
@@ -34,14 +34,28 @@ const Navbar = ({ logout, isAuthenticated }) => {
   );
 
   const authLinks = () => (
-    <li>
-      <a
-        href="/logout"
-        className="text-lg block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-md"
-      >
-        Logout
-      </a>
-    </li>
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-lg block py-2 pr-4 pl-3 md:p-0 bg-cyan-700 text-white dark:text-white md:bg-transparent md:text-cyan-700"
+              : "text-lg block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <a
+          href="/logout"
+          className="text-lg block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-md"
+        >
+          Logout
+        </a>
+      </li>
+    </>
   );
 
   return (
@@ -56,18 +70,6 @@ const Navbar = ({ logout, isAuthenticated }) => {
         <NavbarFlow.Toggle />
       </div>
       <NavbarFlow.Collapse>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "text-lg block py-2 pr-4 pl-3 md:p-0 bg-cyan-700 text-white dark:text-white md:bg-transparent md:text-cyan-700"
-                : "text-lg block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-            }
-          >
-            Home
-          </NavLink>
-        </li>
         {isAuthenticated ? authLinks() : guestLinks()}
       </NavbarFlow.Collapse>
     </NavbarFlow>
