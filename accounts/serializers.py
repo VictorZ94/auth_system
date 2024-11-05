6a25dfe933529222287ad34995dd1333f630f314
Victor Zuluaga Ramirez <49622776+VictorZ94@users.noreply.github.com>
2024-11-04 19:10:32 -0500

    Feat: update docstrig for improving doc

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,6 +5,14 @@ User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
+    """
+    Serializer for creating a new user instance.
+
+    This serializer extends the base UserCreateSerializer to include
+    additional fields specific to the application's user model. It
+    specifies the model to be used and the fields that should be
+    included in the serialized output.
+    """
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'email', 'name', 'password')
